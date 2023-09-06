a003a7f4caf470f7eb171d7f86ee382739c60afd
Ponpeko <128123487+Ponpeko@users.noreply.github.com>
2023-09-06 14:24:02 +0700

    Add files via upload

--- main.cs
+++ main.cs
@@ -3,47 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxiApp;
 
-namespace Calculator
+namespace Main
 {
     class main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Masukkan nilai a: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Masukkan nilai b: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
 
-            Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            //set nilai properties
+            taxi.DriverName = "Juan";
+            taxi.OnDuty = true;
+            taxi.NumPass = 10;
 
+            //pemanggilan method
+            taxi.taxiInfo();
 
-            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            if (taxi.OnDuty == true)
+            {
+                taxi.pickUpPassenger();
+            }
+            else if (taxi.OnDuty == false)
+            {
+                taxi.dropOffPassenger();
+            }
+
             Console.ReadKey();
-        }
-
-
-        static int Penambahan(int a, int b)
-        {
-            return a + b;
-        }
-
-        static int Pengurangan(int a, int b)
-        {
-            return a - b;
-        }
-
-        static int Perkalian(int a, int b)
-        { 
-            return a * b; 
-        } 
-
-        static float Pembagian(float a, float b)
-        {
-            return a / b;
         }
     }
 }
+
