ea51e8009480668b10ab2364b551276a5210a43a
Ponpeko <128123487+Ponpeko@users.noreply.github.com>
2023-09-06 14:34:36 +0700

    Add files via upload

--- main.cs
+++ main.cs
@@ -4,46 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
+namespace PlaneTicket
 {
     class main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Masukkan nilai a: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Masukkan nilai b: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            // object
+            planeTicket ticket = new planeTicket();
+            planeTicket ticket2 = new planeTicket();
 
-            Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, Penambahan(a, b));
-            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            ticket.Origin = "Yogyakarta";
+            ticket.Destination = "PKU";
+            ticket.Cost = 1500000;
+            ticket.Currency = "IDR";
 
+            ticket2.Origin = "Jakarta";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 50;
+            ticket2.Currency = "USD";
 
-            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            ticket.printSummary();
+            ticket2.printSummary();
+
             Console.ReadKey();
-        }
-
-
-        static int Penambahan(int a, int b)
-        {
-            return a + b;
-        }
-
-        static int Pengurangan(int a, int b)
-        {
-            return a - b;
-        }
-
-        static int Perkalian(int a, int b)
-        { 
-            return a * b; 
-        } 
-
-        static float Pembagian(float a, float b)
-        {
-            return a / b;
         }
     }
 }
