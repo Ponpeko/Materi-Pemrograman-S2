bb0b7738c107dfc0193bc8bb68155e2ad2ff576e
Ponpeko <128123487+Ponpeko@users.noreply.github.com>
2023-09-07 13:23:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object baru
+            Weapon sword = new Weapon();
+
+            // mengubah data dari Type (property) dan akan masuk ke type (member class)
+            sword.Type = "Nichirin";  // berhasil
+
+            // menampilkan output
+            sword.display();
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a
+{
+    public class Weapon
+    {
+        // field dengan member class private tidak dapat di akses dari luar
+        private string type;
+
+        // properti untuk mengakses data dari member class
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        // display untuk tes
+        public void display()
+        {
+            Console.WriteLine("Sword type: {0}", type);
+        }
+    }
+}
